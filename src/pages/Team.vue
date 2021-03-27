4589f9fcf8e57639d92869b7af4b247345ef13bc
Tajcore <tahjyeithompson@gmail.com>
2021-03-27 13:21:22 -0500

    Updated Home Page UI and added more team members bio

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -12,11 +12,9 @@
         project which is the main focus of our Capstone Project.
       </div>
     </section>
-    <section
-      class="col-12 justify-center items-center row q-mb-xl q-gutter-xl"
-    >
+    <section class="col-12 justify-center items-center row q-mb-xl q-gutter-xl">
       <TeamCard
-      class="col-md-4 col-sm-9 col-xs-10 col-lg-4"
+        class="col-md-4 col-sm-9 col-xs-10 col-lg-4"
         v-for="member in members"
         :key="member.id"
         :name="member.name"
@@ -36,7 +34,7 @@ const teamMembers = [
   {
     id: 1,
     name: "Tahjyei Thompson",
-    title: "Web Developer / Manga Enthusiast",
+    title: "University Student / Web Developer / Manga Enthusiast",
     bio:
       "Always adapting and never stumbling in this era of technology, I aim to utilize my technical prowess and adaptive nature in order to contribute to the growth and maintenance of organizations’ technical infrastructure.",
     pic: require("../assets/team/tahjyei_thompson.jpg"),
@@ -60,27 +58,28 @@ const teamMembers = [
       instagram: "https://www.instagram.com/znexfh/"
     }
   },
-    {
+  {
     id: 3,
-    name: "Shaun Jennings",
+    name: "Shaun Saint Peter Jennings",
     title: "University Student",
-    bio: "To be added",
+    bio: "Aspiring data scientist",
     pic: require("../assets/team/shaun_jennings.jpeg"),
     socialMedia: {
       instagram: "https://www.instagram.com/shaunjennings_/",
-      github:'https://github.com/ShaunJennings876'
+      github: "https://github.com/ShaunJennings876"
     }
   },
-      {
+  {
     id: 4,
     name: "Brandon Tucker",
     title: "University Student",
-    bio: "To be added",
+    bio:
+      "My current aim is to acquire experience in a professional setting and receive valuable exposure that will enhance my skills and knowledge capabilities.",
     pic: require("../assets/team/brandon_tucker.jpeg"),
     socialMedia: {
       instagram: "https://www.instagram.com/brandontuckerr/",
       linkedin: "https://www.linkedin.com/in/brandon-tucker-603ba9207/",
-      github: 'https://github.com/BeaTea109'
+      github: "https://github.com/BeaTea109"
     }
   }
 ];
